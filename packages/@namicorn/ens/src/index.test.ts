840421c080b0030e997da01bccf3fd3f2ba94fc3
Bogdan Gusiev <agresso@gmail.com>
2019-04-18 16:22:46 +0300

    Fix ENS test

--- packages/@namicorn/ens/src/index.test.ts
+++ packages/@namicorn/ens/src/index.test.ts
@@ -6,11 +6,6 @@ const ens = new ENS({
   // src: new WebsocketProvider('wss://mainnet.infura.io/ws'),
 })
 
-// console.log(
-//   'raw',
-//   (new WebsocketProvider('wss://mainnet.infura.io/ws') as any).sendPayload,
-// )
-
 it('should work', async () => {
   const result = await (ens as any).resolve(
     // 'fdb33f8ac7ce72d7d4795dd8610e323b4c122fbb.addr.reverse',
@@ -23,7 +18,6 @@ it('should work', async () => {
     },
   )
 
-  console.log(result)
-
-  expect(true).toBeTruthy()
+  expect(result.resolver.address).toEqual("0x1da022710df5002339274aadee8d58218e9d6ab5")
+  expect(result.resolver.addr).toEqual("0xd3ddccdd3b25a8a7423b5bee360a42146eb4baf3")
 })
