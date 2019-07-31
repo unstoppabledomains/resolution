f1353e8ef6732850d61214ddb36e6225758545c9
Johnny <johnny@unstoppabledomains.com>
2019-07-31 14:24:11 -0700

    testing the mock http calls for blockchain

--- src/namicorn.test.ts
+++ src/namicorn.test.ts
@@ -51,7 +51,42 @@ it('should work', async () => {
   expect(result).toEqual('0xaa91734f90795e80751c96e682a321bb3c1a4186');
 });
 
-it('resolves .eth name using blockchain', async () => {
+// it('resolves .eth name using blockchain', async () => {
+//   const namicorn = new Namicorn({ blockchain: true });
+//   setTimeCheck({ _scopes: [firstBigCall, secondBigCall], _mstime: 1500 });
+//   const result = await namicorn.address('matthewgould.eth', 'ETH');
+//   expect(result).toEqual('0x714ef33943d925731FBB89C99aF5780D888bD106');
+// });
+
+// import Ens from './ens';
+// it('reverses address to ENS domain', async () => {
+//   const ens = new Ens();
+//   var result = await ens.reverse(
+//     '0xb0E7a465D255aE83eb7F8a50504F3867B945164C',
+//     'ETH',
+//   );
+//   expect(result).toEqual('adrian.argent.xyz');
+//   var result = await ens.reverse(
+//     '0x112234455c3a32fd11230c42e7bccd4a84e02010',
+//     'ETH',
+//   );
+//   expect(result).toEqual(null);
+// });
+
+// it('resolves .xyz name using ENS blockchain', async () => {
+//   const namicorn = new Namicorn({ blockchain: true });
+//   const result = await namicorn.address('adrian.argent.xyz', 'ETH');
+//   expect(result).toEqual('0xb0E7a465D255aE83eb7F8a50504F3867B945164C');
+// });
+
+// it('resolves .luxe name using ENS blockchain', async () => {
+//   const namicorn = new Namicorn({ blockchain: true });
+//   //TODO find luxe domain that resolves
+//   const result = await namicorn.address('bogdantest.luxe', 'ETH');
+//   expect(result).toEqual(null);
+// });
+
+it('resolves .zil name using blockchain', async () => {
   const mockResult = getMockData({
     _ownerAddress: '0x267ca17e8b3bbf49c52a4c3b473cdebcbaf9025e',
   });
@@ -113,51 +148,16 @@ it('resolves .eth name using blockchain', async () => {
     });
 
   const namicorn = new Namicorn({ blockchain: true });
-  setTimeCheck({ _scopes: [firstBigCall, secondBigCall], _mstime: 1500 });
-  const result = await namicorn.address('matthewgould.eth', 'ETH');
-  expect(result).toEqual('0x714ef33943d925731FBB89C99aF5780D888bD106');
+  const result = await namicorn.resolve('cofounding.zil');
+  expect(result.addresses.ETH).toEqual(
+    '0xaa91734f90795e80751c96e682a321bb3c1a4186',
+  );
+  expect(result.meta.owner).toEqual(
+    '0x267ca17e8b3bbf49c52a4c3b473cdebcbaf9025e',
+  );
+  expect(result.meta.type).toEqual('zns');
+  expect(result.meta.ttl).toEqual(0);
 });
-
-// import Ens from './ens';
-// it('reverses address to ENS domain', async () => {
-//   const ens = new Ens();
-//   var result = await ens.reverse(
-//     '0xb0E7a465D255aE83eb7F8a50504F3867B945164C',
-//     'ETH',
-//   );
-//   expect(result).toEqual('adrian.argent.xyz');
-//   var result = await ens.reverse(
-//     '0x112234455c3a32fd11230c42e7bccd4a84e02010',
-//     'ETH',
-//   );
-//   expect(result).toEqual(null);
-// });
-
-// it('resolves .xyz name using ENS blockchain', async () => {
-//   const namicorn = new Namicorn({ blockchain: true });
-//   const result = await namicorn.address('adrian.argent.xyz', 'ETH');
-//   expect(result).toEqual('0xb0E7a465D255aE83eb7F8a50504F3867B945164C');
-// });
-
-// it('resolves .luxe name using ENS blockchain', async () => {
-//   const namicorn = new Namicorn({ blockchain: true });
-//   //TODO find luxe domain that resolves
-//   const result = await namicorn.address('bogdantest.luxe', 'ETH');
-//   expect(result).toEqual(null);
-// });
-
-// it('resolves .zil name using blockchain', async () => {
-//   const namicorn = new Namicorn({ blockchain: true });
-//   const result = await namicorn.resolve('cofounding.zil');
-//   expect(result.addresses.ETH).toEqual(
-//     '0xaa91734f90795e80751c96e682a321bb3c1a4186',
-//   );
-//   expect(result.meta.owner).toEqual(
-//     '0x267ca17e8b3bbf49c52a4c3b473cdebcbaf9025e',
-//   );
-//   expect(result.meta.type).toEqual('zns');
-//   expect(result.meta.ttl).toEqual(0);
-// });
 
 // it('resolves non-existing .zil name using blockchain', async () => {
 //   const namicorn = new Namicorn({ blockchain: true });
