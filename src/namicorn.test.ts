e8b82967c4a9205f629dd486c619176f76798b36
Johnny <johnny@unstoppabledomains.com>
2019-09-12 19:11:42 -0700

    fixed zns after zilliqa update

--- src/namicorn.test.ts
+++ src/namicorn.test.ts
@@ -9,196 +9,196 @@ const MAINNET_URL = 'https://mainnet.infura.io';
 const ZILLIGA_URL = 'https://api.zilliqa.com';
 
 const mockAPICalls = (
-	topLevel: string,
-	testName: string,
-	url = MAINNET_URL,
+  topLevel: string,
+  testName: string,
+  url = MAINNET_URL,
 ) => {
-	const mcdt = mockData as any;
-	const tplvl = mcdt[topLevel] as any;
-	const mockCall = tplvl[testName] as [any];
+  const mcdt = mockData as any;
+  const tplvl = mcdt[topLevel] as any;
+  const mockCall = tplvl[testName] as [any];
 
-	mockCall.forEach(({ METHOD, REQUEST, RESPONSE }) => {
-		switch (METHOD) {
-			case 'POST': {
-				nock(url)
-					// .log(console.log)
-					.post('/', JSON.stringify(REQUEST))
-					.reply(200, JSON.stringify(RESPONSE));
-			}
-			default: {
-				nock(url)
-					// .log(console.log)
-					.get(REQUEST as string)
-					.reply(200, RESPONSE);
-			}
-		}
-	});
+  mockCall.forEach(({ METHOD, REQUEST, RESPONSE }) => {
+    switch (METHOD) {
+      case 'POST': {
+        nock(url)
+          // .log(console.log)
+          .post('/', JSON.stringify(REQUEST))
+          .reply(200, JSON.stringify(RESPONSE));
+      }
+      default: {
+        nock(url)
+          // .log(console.log)
+          .get(REQUEST as string)
+          .reply(200, RESPONSE);
+      }
+    }
+  });
 };
 
 beforeEach(() => {
-	nock.cleanAll();
-	jest.restoreAllMocks();
+  nock.cleanAll();
+  jest.restoreAllMocks();
 });
 
 it('should work', async () => {
-	const testName = 'should work';
-	mockAPICalls('UD_API', testName, DEFAULT_URL);
-	const namicorn = new Namicorn();
-	const result = await namicorn.address('cofounding.zil', 'eth');
-	expect(result).toEqual('0xaa91734f90795e80751c96e682a321bb3c1a4186');
+  const testName = 'should work';
+  mockAPICalls('UD_API', testName, DEFAULT_URL);
+  const namicorn = new Namicorn();
+  const result = await namicorn.address('cofounding.zil', 'eth');
+  expect(result).toEqual('0xaa91734f90795e80751c96e682a321bb3c1a4186');
 });
 
 it('resolves .eth name using blockchain', async () => {
-	const testName = 'resolves .eth name using blockchain';
-	mockAPICalls('ENS', testName, MAINNET_URL);
+  const testName = 'resolves .eth name using blockchain';
+  mockAPICalls('ENS', testName, MAINNET_URL);
 
-	const namicorn = new Namicorn({
-		blockchain: { ens: MAINNET_URL },
-	});
-	var result = await namicorn.address('matthewgould.eth', 'ETH');
-	expect(result).toEqual('0x714ef33943d925731FBB89C99aF5780D888bD106');
+  const namicorn = new Namicorn({
+    blockchain: { ens: MAINNET_URL },
+  });
+  var result = await namicorn.address('matthewgould.eth', 'ETH');
+  expect(result).toEqual('0x714ef33943d925731FBB89C99aF5780D888bD106');
 });
 
 it('reverses address to ENS domain', async () => {
-	const ens = new Ens(MAINNET_URL);
-	const eye = jest
-		.spyOn(ens, '_resolverCallToName')
-		.mockImplementation(() => 'adrian.argent.xyz');
-	const secondEye = jest
-		.spyOn(ens, '_getResolver')
-		.mockImplementation(() => '0xDa1756Bb923Af5d1a05E277CB1E54f1D0A127890');
-	const result = await ens.reverse(
-		'0xb0E7a465D255aE83eb7F8a50504F3867B945164C',
-		'ETH',
-	);
-	expect(eye).toHaveBeenCalled();
-	expect(secondEye).toHaveBeenCalled();
-	expect(result).toEqual('adrian.argent.xyz');
+  const ens = new Ens(MAINNET_URL);
+  const eye = jest
+    .spyOn(ens, '_resolverCallToName')
+    .mockImplementation(() => 'adrian.argent.xyz');
+  const secondEye = jest
+    .spyOn(ens, '_getResolver')
+    .mockImplementation(() => '0xDa1756Bb923Af5d1a05E277CB1E54f1D0A127890');
+  const result = await ens.reverse(
+    '0xb0E7a465D255aE83eb7F8a50504F3867B945164C',
+    'ETH',
+  );
+  expect(eye).toHaveBeenCalled();
+  expect(secondEye).toHaveBeenCalled();
+  expect(result).toEqual('adrian.argent.xyz');
 });
 
 it('reverses address to ENS domain null', async () => {
-	const ens = new Ens(MAINNET_URL);
-	const spy = jest
-		.spyOn(ens, '_getResolver')
-		.mockImplementation(() => '0x0000000000000000000000000000000000000000');
-	const result = await ens.reverse(
-		'0x112234455c3a32fd11230c42e7bccd4a84e02010',
-		'ETH',
-	);
+  const ens = new Ens(MAINNET_URL);
+  const spy = jest
+    .spyOn(ens, '_getResolver')
+    .mockImplementation(() => '0x0000000000000000000000000000000000000000');
+  const result = await ens.reverse(
+    '0x112234455c3a32fd11230c42e7bccd4a84e02010',
+    'ETH',
+  );
 
-	expect(spy).toHaveBeenCalled();
-	expect(result).toEqual(null);
+  expect(spy).toHaveBeenCalled();
+  expect(result).toEqual(null);
 });
 
 it('resolves .xyz name using ENS blockchain', async () => {
-	const namicorn = new Namicorn({
-		blockchain: { ens: MAINNET_URL },
-	});
+  const namicorn = new Namicorn({
+    blockchain: { ens: MAINNET_URL },
+  });
 
-	const spy = jest
-		.spyOn(namicorn.ens, '_getResolutionInfo')
-		.mockImplementation(() =>
-			Promise.resolve([
-				'0xb0E7a465D255aE83eb7F8a50504F3867B945164C',
-				Number(0x00),
-				'0xDa1756Bb923Af5d1a05E277CB1E54f1D0A127890',
-			]),
-		);
+  const spy = jest
+    .spyOn(namicorn.ens, '_getResolutionInfo')
+    .mockImplementation(() =>
+      Promise.resolve([
+        '0xb0E7a465D255aE83eb7F8a50504F3867B945164C',
+        Number(0x00),
+        '0xDa1756Bb923Af5d1a05E277CB1E54f1D0A127890',
+      ]),
+    );
 
-	const secondSpy = jest
-		.spyOn(namicorn.ens, 'fetchAddress')
-		.mockImplementation(() =>
-			Promise.resolve('0xb0E7a465D255aE83eb7F8a50504F3867B945164C'),
-		);
+  const secondSpy = jest
+    .spyOn(namicorn.ens, 'fetchAddress')
+    .mockImplementation(() =>
+      Promise.resolve('0xb0E7a465D255aE83eb7F8a50504F3867B945164C'),
+    );
 
-	const result = await namicorn.address('adrian.argent.xyz', 'ETH');
-	expect(spy).toBeCalled();
-	expect(secondSpy).toBeCalled();
-	expect(result).toEqual('0xb0E7a465D255aE83eb7F8a50504F3867B945164C');
+  const result = await namicorn.address('adrian.argent.xyz', 'ETH');
+  expect(spy).toBeCalled();
+  expect(secondSpy).toBeCalled();
+  expect(result).toEqual('0xb0E7a465D255aE83eb7F8a50504F3867B945164C');
 });
 
 it('resolves .luxe name using ENS blockchain', async () => {
-	const namicorn = new Namicorn({
-		blockchain: { ens: MAINNET_URL },
-	});
+  const namicorn = new Namicorn({
+    blockchain: { ens: MAINNET_URL },
+  });
 
-	const spy = jest
-		.spyOn(namicorn.ens, '_getResolutionInfo')
-		.mockImplementation(() =>
-			Promise.resolve([
-				'0xf3dE750A73C11a6a2863761E930BF5fE979d5663',
-				Number(0x00),
-				'0xBD5F5ec7ed5f19b53726344540296C02584A5237',
-			]),
-		);
+  const spy = jest
+    .spyOn(namicorn.ens, '_getResolutionInfo')
+    .mockImplementation(() =>
+      Promise.resolve([
+        '0xf3dE750A73C11a6a2863761E930BF5fE979d5663',
+        Number(0x00),
+        '0xBD5F5ec7ed5f19b53726344540296C02584A5237',
+      ]),
+    );
 
-	const secondSpy = jest
-		.spyOn(namicorn.ens, 'fetchAddress')
-		.mockImplementation(() =>
-			Promise.resolve('0xf3dE750A73C11a6a2863761E930BF5fE979d5663'),
-		);
+  const secondSpy = jest
+    .spyOn(namicorn.ens, 'fetchAddress')
+    .mockImplementation(() =>
+      Promise.resolve('0xf3dE750A73C11a6a2863761E930BF5fE979d5663'),
+    );
 
-	const result = await namicorn.address('john.luxe', 'ETH');
-	expect(spy).toBeCalled();
-	expect(secondSpy).toBeCalled();
-	expect(result).toEqual('0xf3dE750A73C11a6a2863761E930BF5fE979d5663');
+  const result = await namicorn.address('john.luxe', 'ETH');
+  expect(spy).toBeCalled();
+  expect(secondSpy).toBeCalled();
+  expect(result).toEqual('0xf3dE750A73C11a6a2863761E930BF5fE979d5663');
 });
 
 it('resolves .luxe name using ENS blockchain null', async () => {
-	const namicorn = new Namicorn({
-		blockchain: { ens: MAINNET_URL },
-	});
+  const namicorn = new Namicorn({
+    blockchain: { ens: MAINNET_URL },
+  });
 
-	const spy = jest
-		.spyOn(namicorn.ens, '_getResolutionInfo')
-		.mockImplementation(() =>
-			Promise.resolve([
-				'0x0000000000000000000000000000000000000000',
-				Number(0x00),
-				'0x0000000000000000000000000000000000000000',
-			]),
-		);
+  const spy = jest
+    .spyOn(namicorn.ens, '_getResolutionInfo')
+    .mockImplementation(() =>
+      Promise.resolve([
+        '0x0000000000000000000000000000000000000000',
+        Number(0x00),
+        '0x0000000000000000000000000000000000000000',
+      ]),
+    );
 
-	const secondSpy = jest
-		.spyOn(namicorn.ens, 'fetchAddress')
-		.mockImplementation(() => Promise.resolve(null));
+  const secondSpy = jest
+    .spyOn(namicorn.ens, 'fetchAddress')
+    .mockImplementation(() => Promise.resolve(null));
 
-	const result = await namicorn.address('something.luxe', 'ETH');
-	expect(spy).toBeCalled();
-	expect(secondSpy).toBeCalled();
-	expect(result).toEqual(null);
+  const result = await namicorn.address('something.luxe', 'ETH');
+  expect(spy).toBeCalled();
+  expect(secondSpy).toBeCalled();
+  expect(result).toEqual(null);
 });
 
 it('resolves .zil name using blockchain', async () => {
-	const testName = 'resolves .zil name using blockchain';
-	// mockAPICalls('ZIL', testName, ZILLIGA_URL);
-	const namicorn = new Namicorn({ blockchain: { zns: ZILLIGA_URL } });
-	const result = await namicorn.resolve('cofounding.zil');
-	expect(result).toBeDefined();
-	expect(result && result.addresses.ETH).toEqual(
-		'0xaa91734f90795e80751c96e682a321bb3c1a4186',
-	);
-	expect(result && result.meta.owner).toEqual(
-		'0x267ca17e8b3bbf49c52a4c3b473cdebcbaf9025e',
-	);
-	expect(result && result.meta.type).toEqual('zns');
-	expect(result && result.meta.ttl).toEqual(0);
+  const testName = 'resolves .zil name using blockchain';
+  // mockAPICalls('ZIL', testName, ZILLIGA_URL);
+  const namicorn = new Namicorn({ blockchain: { zns: ZILLIGA_URL } });
+  const result = await namicorn.resolve('cofounding.zil');
+  expect(result).toBeDefined();
+  expect(result && result.addresses.ETH).toEqual(
+    '0xaa91734f90795e80751c96e682a321bb3c1a4186',
+  );
+  expect(result && result.meta.owner).toEqual(
+    '0x267ca17e8b3bbf49c52a4c3b473cdebcbaf9025e',
+  );
+  expect(result && result.meta.type).toEqual('zns');
+  expect(result && result.meta.ttl).toEqual(0);
 });
 
 it('provides empty response constant', async () => {
-	const response = Namicorn.UNCLAIMED_DOMAIN_RESPONSE;
-	expect(response.addresses).toEqual({});
-	expect(response.meta.owner).toEqual(null);
+  const response = Namicorn.UNCLAIMED_DOMAIN_RESPONSE;
+  expect(response.addresses).toEqual({});
+  expect(response.meta.owner).toEqual(null);
 });
 
 it('resolves non-existing domain zone', async () => {
-	const namicorn = new Namicorn({ blockchain: true });
-	const result = await namicorn.address('bogdangusiev.qq', 'ZIL');
-	expect(result).toEqual(null);
+  const namicorn = new Namicorn({ blockchain: true });
+  const result = await namicorn.address('bogdangusiev.qq', 'ZIL');
+  expect(result).toEqual(null);
 });
 
 xit('resolves rsk name using blockchain', async () => {
-	const namicorn = new Namicorn({ blockchain: true });
-	const result = await namicorn.address('alice.rsk', 'ETH');
-	expect(result).toEqual('0xd96d39c91b3d0236437e800f874800b026dc5f14');
+  const namicorn = new Namicorn({ blockchain: true });
+  const result = await namicorn.address('alice.rsk', 'ETH');
+  expect(result).toEqual('0xd96d39c91b3d0236437e800f874800b026dc5f14');
 });
--- src/zns.ts
+++ src/zns.ts
@@ -11,84 +11,83 @@ const NULL_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 type ResolverRecord = { key: string; val: string };
 type ResolverRecordsStructure = {
-	crypto?: { [key: string]: { address: string } };
-	ttl?: string;
-	[key: string]: any;
+  crypto?: { [key: string]: { address: string } };
+  ttl?: string;
+  [key: string]: any;
 };
 
 type ZNSResolve = {
-	crypto?: { [key: string]: { address: string } };
-	ttl?: string;
-	[key: string]: any;
+  crypto?: { [key: string]: { address: string } };
+  ttl?: string;
+  [key: string]: any;
 };
 
 export default class {
-	registry: Contract;
-	zilliqa: Zilliqa;
+  registry: Contract;
+  zilliqa: Zilliqa;
 
-	constructor(source: string | boolean = DEFAULT_SOURCE) {
-		if (source == true) {
-			source = DEFAULT_SOURCE;
-		}
-		source = source.toString();
-		this.zilliqa = new Zilliqa(source);
-		this.registry = this.zilliqa.contracts.at(registryAddress);
-	}
+  constructor(source: string | boolean = DEFAULT_SOURCE) {
+    if (source == true) {
+      source = DEFAULT_SOURCE;
+    }
+    source = source.toString();
+    this.zilliqa = new Zilliqa(source);
+    this.registry = this.zilliqa.contracts.at(registryAddress);
+  }
 
-	async getContractField(contract: Contract, field: string): Promise<any> {
-		const state = await contract.getState();
-		const fs = require('fs');
-		fs.writeFileSync(`./${field}.json`, JSON.stringify(state));
-		return state && state[field as any];
-	}
+  async getContractField(contract: Contract, field: string): Promise<any> {
+    const state = await contract.getState();
+    const fs = require('fs');
+    fs.writeFileSync(`./${field}.json`, JSON.stringify(state));
+    return state && state[field as any];
+  }
 
-	async getResolverRecordsStructure(
-		resolverAddress: string,
-	): Promise<ResolverRecordsStructure> {
-		if (resolverAddress == NULL_ADDRESS) {
-			return {};
-		}
-		const resolver = this.zilliqa.contracts.at(
-			toChecksumAddress(resolverAddress),
-		);
-		const resolverRecords =
-			((await this.getContractField(
-				resolver,
-				'records',
-			)) as any);
-		const result = {};
-		Object.keys(resolverRecords).forEach(recordKey => {
-			_.set(result, recordKey, resolverRecords[recordKey]);
-		})
-		// resolverRecords.forEach(record => {
-		// 	_.set(result, record.key, record.val);
-		// });
-		return result;
-	}
+  async getResolverRecordsStructure(
+    resolverAddress: string,
+  ): Promise<ResolverRecordsStructure> {
+    if (resolverAddress == NULL_ADDRESS) {
+      return {};
+    }
+    const resolver = this.zilliqa.contracts.at(
+      toChecksumAddress(resolverAddress),
+    );
+    const resolverRecords = (await this.getContractField(
+      resolver,
+      'records',
+    )) as any;
+    const result = {};
+    Object.keys(resolverRecords).forEach(recordKey => {
+      _.set(result, recordKey, resolverRecords[recordKey]);
+    });
+    // resolverRecords.forEach(record => {
+    // 	_.set(result, record.key, record.val);
+    // });
+    return result;
+  }
 
-	async resolve(domain: string | undefined): Promise<ZNSResolve | null> {
-		const nodeHash = namehash(domain);
-		const registryRecords = await this.getContractField(
-			this.registry,
-			'records',
-		);
+  async resolve(domain: string | undefined): Promise<ZNSResolve | null> {
+    const nodeHash = namehash(domain);
+    const registryRecords = await this.getContractField(
+      this.registry,
+      'records',
+    );
 
-		if (!registryRecords) return null;
-		const registryRecord = registryRecords[nodeHash]
-		if (!registryRecord) return null;
-		const [ownerAddress, resolverAddress] = registryRecord.arguments as [
-			string,
-			string
-		];
-		const resolution = await this.getResolverRecordsStructure(resolverAddress);
-		const addresses = _.mapValues(resolution.crypto, 'address');
-		return {
-			addresses,
-			meta: {
-				owner: ownerAddress || null,
-				type: 'zns',
-				ttl: parseInt(resolution.ttl as string) || 0,
-			},
-		};
-	}
+    if (!registryRecords) return null;
+    const registryRecord = registryRecords[nodeHash];
+    if (!registryRecord) return null;
+    const [ownerAddress, resolverAddress] = registryRecord.arguments as [
+      string,
+      string
+    ];
+    const resolution = await this.getResolverRecordsStructure(resolverAddress);
+    const addresses = _.mapValues(resolution.crypto, 'address');
+    return {
+      addresses,
+      meta: {
+        owner: ownerAddress || null,
+        type: 'zns',
+        ttl: parseInt(resolution.ttl as string) || 0,
+      },
+    };
+  }
 }
