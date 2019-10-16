06f9d00c645caac8db43a2100413e98cc7ff0b04
Bogdan Gusiev <agresso@gmail.com>
2019-10-16 19:56:00 +0300

    Renaming

--- src/NamingService.ts
+++ src/NamingService.ts
@@ -1,6 +1,8 @@
 import { SourceDefinition } from './types';
 
 export default abstract class NamingService {
+  abstract isSupportedDomain(): boolean
+  abstract isSupportedNetwork(): boolean;
   protected abstract normalizeSource(
     source: boolean | string | SourceDefinition,
   );
