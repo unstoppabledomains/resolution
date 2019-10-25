ba4b6179d82f500baffcd06a1199a41399d94717
Johnny <johnny@unstoppabledomains.com>
2019-10-24 18:34:02 -0700

    * Expanded NamingService to contain resolution(domain:string)

--- src/NamingService.ts
+++ src/NamingService.ts
@@ -3,6 +3,7 @@ import { SourceDefinition } from './types';
 export default abstract class NamingService {
   abstract isSupportedDomain(domain: string): boolean;
   abstract isSupportedNetwork(): boolean;
+  abstract resolution(domain: string): Promise<Object | null>;
   protected abstract normalizeSource(
     source: boolean | string | SourceDefinition,
   );
