2e92423007e2579049dcf12aeaf82c6da7707373
Johnny <johnny@unstoppabledomains.com>
2019-10-30 15:20:51 -0700

    hot fix rename file to be smalcase

---
+++ src/namingService.ts
@@ -0,0 +1,14 @@
+import { SourceDefinition } from './types';
+
+/**
+ * Abstract class for different Naming Service supports like
+ * - ENS
+ * - ZNS
+ */
+export default abstract class NamingService {
+  abstract isSupportedDomain(domain: string): boolean;
+  abstract isSupportedNetwork(): boolean;
+  protected abstract normalizeSource(
+    source: boolean | string | SourceDefinition,
+  );
+}
