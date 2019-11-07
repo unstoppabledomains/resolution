5d5067e13df39c1f78247d79f684fdb7b9c7ff96
Johnny <johnny@unstoppabledomains.com>
2019-11-07 03:17:54 -0800

    deploy scripts

--- predocs.js
+++ predocs.js
@@ -4,7 +4,19 @@ const pckg = JSON.parse(fs.readFileSync('./package.json'));
 const version = pckg.version;
 
 const tsconfig = JSON.parse(fs.readFileSync('./tsconfig.json'));
-console.log({test: tsconfig.typedocOptions.out});
-tsconfig.typedocOptions.out = `./formal-docs/v${version}`;
-console.log({test: tsconfig.typedocOptions.out});
+tsconfig.typedocOptions.out = `./dist/v${version}`;
 fs.writeFileSync('./tsconfig.json', JSON.stringify(tsconfig));
+
+let README = fs.readFileSync('./dist/README.md', 'utf8');
+
+const updateCurrentLink = (readme, version) => {
+  const draft = /\[(Current Version)\]\((.+)\)/;
+  const currentVersionReg = /\/(v\d+?[.]\d+?[.]\d+?)\//;
+  const link = readme.match(draft)[2];
+  const currentVersion = link.match(currentVersionReg);
+  const newLink = link.replace(currentVersion[1], version);
+  const newReadme = readme.replace(link, newLink);
+  return newReadme;
+}
+README = updateCurrentLink(README, `v${version}`);
+fs.writeFileSync('./dist/README.md', README);
