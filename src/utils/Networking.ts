3266d0b551785d485a99f3a672e503647dc6d473
Ryan Le <ryan@buyethdomains.com>
2021-09-17 06:17:39 -0500

    Replaces node-fetch with cross-fetch (#183)

--- src/utils/Networking.ts
+++ src/utils/Networking.ts
@@ -1,28 +1,10 @@
-import nodeFetch, {Response as FetchResponse} from 'node-fetch';
+import crossFetch from 'cross-fetch';
 
 export default class Networking {
-  static isNode(): boolean {
-    if (typeof process === 'object') {
-      // eslint-disable-next-line no-undef
-      if (typeof process.versions === 'object') {
-        // eslint-disable-next-line no-undef
-        if (typeof process.versions.node !== 'undefined') {
-          return true;
-        }
-      }
-    }
-
-    return false;
-  }
-
-  static getEnv(): 'NODE' | 'BROWSER' {
-    return this.isNode() ? 'NODE' : 'BROWSER';
-  }
-
   static async fetch(
     url: string,
     options: {body?: string; headers?: Record<string, string>; method?: string},
-  ): Promise<FetchResponse | Response> {
-    return this.isNode() ? nodeFetch(url, options) : window.fetch(url, options);
+  ): Promise<Response> {
+    return crossFetch(url, options);
   }
 }
